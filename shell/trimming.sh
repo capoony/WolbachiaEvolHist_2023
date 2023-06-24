0e7802012c105e0be87d7049317dadf633da4f44
capoony <capoony@gmail.com>
2023-06-24 08:32:57 +0200

    update

--- shell/trimming.sh
+++ shell/trimming.sh
@@ -44,3 +44,17 @@ cd /media/inter/mkapun/projects/WolbachiaEvolHist_2023/data/trim
 ## loop through all FASTQ pairs and trim by quality PHRED 20, min length 85bp and automatically detect & remove adapters
 
 trim_galore --quality 20 --length 30 --cores 20 --fastqc --gzip /media/inter/mkapun/projects/WolbachiaEvolHist_2023/data/reads/${ID}.fastq.gz
+
+## unzip FASTQC folders to get readlengths
+cd /media/inter/mkapun/projects/WolbachiaEvolHist_2023/data/trim
+
+for i in *.zip; do unzip $i; done
+
+for ID in 380 HG_16 HG_14 HG_17 HG_18 HG_19 HG_21 HG0021 HG0025 HG0028 HG0035 HG29702 HG47203 376 377 378 HG_09 HG_15 HG_16 HG_20 HG0026 HG0027 HG0029 HG0034 HG47203 HG47204 HG47205 CK2 DGRP335 DGRP338 ED2 ED3 ED6N ED10N EZ2 GA125 KN34 KR7 RG3 RG5 RG34 SP80 TZ14 UG5N ZI268 ZO12 ZS11; do
+
+    python /media/inter/mkapun/projects/WolbachiaEvolHist_2023/scripts/fastqLengthDist.py \
+        --input /media/inter/mkapun/projects/WolbachiaEvolHist_2023/data/trim/${ID}_1_val_1_fastqc/fastqc_data.txt \
+        --name ${ID} \
+        >>/media/inter/mkapun/projects/WolbachiaEvolHist_2023/output/TrimmedReadLength.summary
+
+done
